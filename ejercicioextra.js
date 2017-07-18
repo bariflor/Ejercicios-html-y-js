e2d33cb6dbd66b3e26a7d48b6af1d43557799df8
bariflor <barihuamachuco@gmail.com>
2017-07-18 14:36:09 -0500

    modificado ejercicio 3

--- ejercicioextra.js
+++ ejercicioextra.js
@@ -1,11 +1,8 @@
 
-function convertirCelsius(){
-	var x;
-    if (degree == "C") {
-        x = document.getElementById("c").value * 9 / 5 + 32;
-        document.getElementById("f").value = Math.round(x);
-    } else {
-        x = (document.getElementById("f").value -32) * 5 / 9;
-        document.getElementById("c").value = Math.round(x);
-    }
+function convertirCelcius(){
+	
+	var num = document.getElementById("nro").value;
+
+	rpta.innerText = num * 9 / 5 + 32;
+
 }
